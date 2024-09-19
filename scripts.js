95e06bc8896fc6e5d73b3284dde50bc6efe08f44
timboobimboo <hthh1482@gmail.com>
2024-09-19 15:25:23 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
+// INTRO DYNAMIC TYPEWRITER EFFECT
+
 const intro = document.getElementById('intro');
 const introP = intro.children;
 
-// DYNAMIC TYPEWRITER EFFECT
+
 for (let i=0; i < introP.length ; i++) {
 
     var characterCount = introP[i].innerHTML.length;
@@ -10,33 +12,31 @@ for (let i=0; i < introP.length ; i++) {
 
     var prevSib = introP[i].previousElementSibling;
 
-    introP[i].style.animationDuration = animationTime + 's';
-    introP[i].style.animationTimingFunction = 'steps(' + characterCount + ')';
+    introP[i].style.width = '0%';
+    introP[i].style.animation = 'typing ' + animationTime + 's steps(' + characterCount + '), cursor .7s step-end infinite alternate'
+    introP[i].style.animationDelay = '1s';
+    introP[i].style.animationFillMode = 'forwards';
+    // introP[i].style.animationDuration = animationTime + 's';
+    // introP[i].style.animationTimingFunction = 'steps(' + characterCount + ')';
+
 
     if (i > 0)  {
-        introP[i].style.border = '0'
+        introP[i].style.border = '0';
         introP[i].style.animationPlayState  = 'paused';
         
-
         prevSib.addEventListener("animationend", animStart)
-        introP[1].addEventListener("animationend", delCur)
-
-        function delCur() {
-            introP[i].style.borderRight = '0';
-        }
     
         function animStart() {
-            // introP[i].style.animationDelay = '1s';
             introP[i].style.borderRight = '0.1em solid';
             introP[i].style.animationPlayState = 'running';
         }
+    }    
 
-    } else {    introP[1].addEventListener("animationend", delCur)
+    introP[i].addEventListener("animationend", delCur)
 
-        function delCur() {
-            introP[i].style.borderRight = '0';
-        }
-    
+    function delCur() {
+        introP[i].style.borderRight = '0';
     }
+
     console.log(prevSib)
 }
